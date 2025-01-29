760896255ed79e70fefd5553065231933117a32f
Tee Akpata <teeakpata@Tees-MacBook-Air.local>
2025-01-29 18:36:01 +0100

    dependencies installation

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3001;
 app.get('/api/v1/info', (req, res) => {
     const currentDatetime = new Date().toISOString();
     const resp = {
-        email: 'thelma101@gmail.com',
+        email: process.env.EMAIL,
         current_datetime: currentDatetime,
         github_url: 'https://github.com/Thelma101/stage0-publicAPI'
     }
